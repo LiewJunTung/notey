883d6d62e2f8a2b7edf8ff1cb6da243b88f5cbb3
JT <pandawarrior91@gmail.com>
2013-10-17 04:44:46 +0800

    3rd edit with proper ui and alarm

--- todo.js
+++ todo.js
@@ -1,49 +1,128 @@
-function Search()
+function Main()
 {
-	var text = document.getElementById('notes').value;
+        var text = document.getElementById('task').value;
 
-	var compareCall = text.search(/call/i);
-	var compareSMS = text.search(/sms/i);
+		var phone_name = text.split(' ');
+		var phone_number = phone_name[1];
 		
-	if (compareCall == 0)
-		Phonecall();
-	else if (compareSMS === 0)
-		SMS();
-	else 
-		alert("Nope!");
-}	
+        var compareCall = text.search(/call/i);
+        var compareSMS = text.search(/sms/i);
+        var compareTest = text.search(/test/i);
+        var compareTime = text.search(/@/i);
 
-function SMS()
+        if(compareTime != -1)
+        {
+            if(compareSMS == 0)
+                ph = "SMS";
+            else if(compareCall == 0)
+                ph = "Phone";
+            else
+            {
+                ph = "none";
+                phone_number = "none"
+            }
+
+        var n=text.indexOf("@");
+        var len = text.length;
+        var time = text.slice(n+2,len);                     // 2 spaces after keyword "@" and the end of the string =  Desired Time.
+        var time2 = "October 17, 2013 "+ time;
+        var myDate  = new Date (time2);
+
+        // The "honorTimezone" string is what make the alarm honoring it
+        var request = navigator.mozAlarms.add(myDate, "ignoreTimezone", Notification(text, ph, phone_number));
+
+        request.onsuccess = function () {
+        alert("The alarm has been scheduled");
+        };
+
+        request.onerror = function () {
+          alert("An error occurred: " + this.error.name);
+        };           
+        }
+        else 
+            alert("Nope!");
+}        
+
+    function Notification(text, ph, phone_number){
+           navigator.mozSetMessageHandler("alarm", function (mozAlarm) {
+           var notification = navigator.mozNotification.createNotification(
+               "You got a reminder!",
+                text
+            );
+
+        notification.onclick = function onclick() {
+           if (ph == "SMS")
+                SMS(phone_number);
+            else if (ph == "Phone")
+                Phonecall(phone_number);
+            else;
+        };
+
+        notification.show();
+         });
+        }
+
+function SMS(phone_number)
 {
 
      var sms = new MozActivity(
      {
-     	name: "new", // Possible compose-sms in future versions
-     	data: {
-     	type: "websms/sms",
-     	number: "+0173668195"
+             name: "new", // Possible compose-sms in future versions
+             data: {
+             type: "websms/sms",
+             number: "+0173668195"
      }
      });
 }
 
-function Phonecall()
+function Phonecall(phone_number)
 {
             var call = new MozActivity({
                 name: "dial",
                 data: {
-                    number: "+46777888999"
+                    number: "+467799"
                 }
             });
 }
 
 
-function Call()
+function Search()
 {
+var filter = {
+  filterValue : "zorro",
+  filterBy    : ["name"],
+  filterOp    : "contains",
+  filterLimit : 1
+}
+var search = navigator.mozContacts.find(filter);
+
+search.onsuccess = function() {
+  if (search.result.length === 1) {
+    var person = search.result[0];
+    alert("Found:" + person.givenName[0] + " " + person.familyName[0]);
+  } else {
+    alert("Sorry, there is no such contact.")
+  }
+}
+
+search.onerror = function() {
+  alert("Uh! Something goes wrong, no result found!");
+}
+
+}
+
+function Cancel()
+{
+    window.location = 'Notey.html';
 }
 
  document.addEventListener('DOMContentLoaded', function () {
-         var link = document.getElementById('notebutton');
+         var link = document.getElementById('btn-add');
            link.addEventListener('click', function(){          
-           Search();
+           Main();
+           })
+        var link2 = document.getElementById('btn-cancel');
+           link2.addEventListener('click', function(){          
+           Cancel();
            })
  })
